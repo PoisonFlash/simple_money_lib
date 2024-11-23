dcd88d29eb3ced06a4e2ce0506f4a467e69349e5
PoisonFlash <128981512+PoisonFlash@users.noreply.github.com>
2024-11-23 10:00:42 +0100

    Currency rework ongoing. Next: consider dynamic resolver as the next logical step instead of full dev-side generation.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="simple_money_lib",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),  # Automatically find and include the package files
     install_requires=["py-moneyed"],
     extras_require={
