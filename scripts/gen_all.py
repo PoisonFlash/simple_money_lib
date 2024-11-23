b276baf88a31040db31aebc77bc568508484e3b3
PoisonFlash <128981512+PoisonFlash@users.noreply.github.com>
2024-11-23 10:06:53 +0100

    Currency rework ongoing. Next: consider dynamic resolver as the next logical step instead of full dev-side generation.

---
+++ scripts/gen_all.py
@@ -0,0 +1,361 @@
+from simple_money_lib.currency import Currency
+
+AED = Currency('AED', 784, 2, 'United Arab Emirates dirham')
+AFN = Currency('AFN', 971, 2, 'Afghan afghani')
+ALL = Currency('ALL', 8, 2, 'Albanian lek')
+AMD = Currency('AMD', 51, 2, 'Armenian dram')
+ANG = Currency('ANG', 532, 2, 'Netherlands Antillean guilder')
+AOA = Currency('AOA', 973, 2, 'Angolan kwanza')
+ARS = Currency('ARS', 32, 2, 'Argentine peso')
+AUD = Currency('AUD', 36, 2, 'Australian dollar')
+AWG = Currency('AWG', 533, 2, 'Aruban florin')
+AZN = Currency('AZN', 944, 2, 'Azerbaijani manat')
+BAM = Currency('BAM', 977, 2, 'Bosnia and Herzegovina convertible mark')
+BBD = Currency('BBD', 52, 2, 'Barbados dollar')
+BDT = Currency('BDT', 50, 2, 'Bangladeshi taka')
+BGN = Currency('BGN', 975, 2, 'Bulgarian lev')
+BHD = Currency('BHD', 48, 3, 'Bahraini dinar')
+BIF = Currency('BIF', 108, 0, 'Burundian franc')
+BMD = Currency('BMD', 60, 2, 'Bermudian dollar')
+BND = Currency('BND', 96, 2, 'Brunei dollar')
+BOB = Currency('BOB', 68, 2, 'Boliviano')
+BOV = Currency('BOV', 984, 2, 'Bolivian Mvdol (funds code)')
+BRL = Currency('BRL', 986, 2, 'Brazilian real')
+BSD = Currency('BSD', 44, 2, 'Bahamian dollar')
+BTN = Currency('BTN', 64, 2, 'Bhutanese ngultrum')
+BWP = Currency('BWP', 72, 2, 'Botswana pula')
+BYN = Currency('BYN', 933, 2, 'Belarusian ruble')
+BZD = Currency('BZD', 84, 2, 'Belize dollar')
+CAD = Currency('CAD', 124, 2, 'Canadian dollar')
+CDF = Currency('CDF', 976, 2, 'Congolese franc')
+CHE = Currency('CHE', 947, 2, 'WIR euro (complementary currency)')
+CHF = Currency('CHF', 756, 2, 'Swiss franc')
+CHW = Currency('CHW', 948, 2, 'WIR franc (complementary currency)')
+CLF = Currency('CLF', 990, 4, 'Unidad de Fomento (funds code)')
+CLP = Currency('CLP', 152, 0, 'Chilean peso')
+CNY = Currency('CNY', 156, 2, 'Renminbi')
+COP = Currency('COP', 170, 2, 'Colombian peso')
+COU = Currency('COU', 970, 2, 'Unidad de Valor Real (UVR) (funds code)')
+CRC = Currency('CRC', 188, 2, 'Costa Rican colon')
+CUP = Currency('CUP', 192, 2, 'Cuban peso')
+CVE = Currency('CVE', 132, 2, 'Cape Verdean escudo')
+CZK = Currency('CZK', 203, 2, 'Czech koruna')
+DJF = Currency('DJF', 262, 0, 'Djiboutian franc')
+DKK = Currency('DKK', 208, 2, 'Danish krone')
+DOP = Currency('DOP', 214, 2, 'Dominican peso')
+DZD = Currency('DZD', 12, 2, 'Algerian dinar')
+EGP = Currency('EGP', 818, 2, 'Egyptian pound')
+ERN = Currency('ERN', 232, 2, 'Eritrean nakfa')
+ETB = Currency('ETB', 230, 2, 'Ethiopian birr')
+EUR = Currency('EUR', 978, 2, 'Euro')
+FJD = Currency('FJD', 242, 2, 'Fiji dollar')
+FKP = Currency('FKP', 238, 2, 'Falkland Islands pound')
+GBP = Currency('GBP', 826, 2, 'Pound sterling')
+GEL = Currency('GEL', 981, 2, 'Georgian lari')
+GHS = Currency('GHS', 936, 2, 'Ghanaian cedi')
+GIP = Currency('GIP', 292, 2, 'Gibraltar pound')
+GMD = Currency('GMD', 270, 2, 'Gambian dalasi')
+GNF = Currency('GNF', 324, 0, 'Guinean franc')
+GTQ = Currency('GTQ', 320, 2, 'Guatemalan quetzal')
+GYD = Currency('GYD', 328, 2, 'Guyanese dollar')
+HKD = Currency('HKD', 344, 2, 'Hong Kong dollar')
+HNL = Currency('HNL', 340, 2, 'Honduran lempira')
+HTG = Currency('HTG', 332, 2, 'Haitian gourde')
+HUF = Currency('HUF', 348, 2, 'Hungarian forint')
+IDR = Currency('IDR', 360, 2, 'Indonesian rupiah')
+ILS = Currency('ILS', 376, 2, 'Israeli new shekel')
+INR = Currency('INR', 356, 2, 'Indian rupee')
+IQD = Currency('IQD', 368, 3, 'Iraqi dinar')
+IRR = Currency('IRR', 364, 2, 'Iranian rial')
+ISK = Currency('ISK', 352, 0, 'Icelandic króna (plural: krónur)')
+JMD = Currency('JMD', 388, 2, 'Jamaican dollar')
+JOD = Currency('JOD', 400, 3, 'Jordanian dinar')
+JPY = Currency('JPY', 392, 0, 'Japanese yen')
+KES = Currency('KES', 404, 2, 'Kenyan shilling')
+KGS = Currency('KGS', 417, 2, 'Kyrgyzstani som')
+KHR = Currency('KHR', 116, 2, 'Cambodian riel')
+KMF = Currency('KMF', 174, 0, 'Comoro franc')
+KPW = Currency('KPW', 408, 2, 'North Korean won')
+KRW = Currency('KRW', 410, 0, 'South Korean won')
+KWD = Currency('KWD', 414, 3, 'Kuwaiti dinar')
+KYD = Currency('KYD', 136, 2, 'Cayman Islands dollar')
+KZT = Currency('KZT', 398, 2, 'Kazakhstani tenge')
+LAK = Currency('LAK', 418, 2, 'Lao kip')
+LBP = Currency('LBP', 422, 2, 'Lebanese pound')
+LKR = Currency('LKR', 144, 2, 'Sri Lankan rupee')
+LRD = Currency('LRD', 430, 2, 'Liberian dollar')
+LSL = Currency('LSL', 426, 2, 'Lesotho loti')
+LYD = Currency('LYD', 434, 3, 'Libyan dinar')
+MAD = Currency('MAD', 504, 2, 'Moroccan dirham')
+MDL = Currency('MDL', 498, 2, 'Moldovan leu')
+MGA = Currency('MGA', 969, 2, 'Malagasy ariary')
+MKD = Currency('MKD', 807, 2, 'Macedonian denar')
+MMK = Currency('MMK', 104, 2, 'Myanmar kyat')
+MNT = Currency('MNT', 496, 2, 'Mongolian tögrög')
+MOP = Currency('MOP', 446, 2, 'Macanese pataca')
+MRU = Currency('MRU', 929, 2, 'Mauritanian ouguiya')
+MUR = Currency('MUR', 480, 2, 'Mauritian rupee')
+MVR = Currency('MVR', 462, 2, 'Maldivian rufiyaa')
+MWK = Currency('MWK', 454, 2, 'Malawian kwacha')
+MXN = Currency('MXN', 484, 2, 'Mexican peso')
+MXV = Currency('MXV', 979, 2, 'Mexican Unidad de Inversion (UDI) (funds code)')
+MYR = Currency('MYR', 458, 2, 'Malaysian ringgit')
+MZN = Currency('MZN', 943, 2, 'Mozambican metical')
+NAD = Currency('NAD', 516, 2, 'Namibian dollar')
+NGN = Currency('NGN', 566, 2, 'Nigerian naira')
+NIO = Currency('NIO', 558, 2, 'Nicaraguan córdoba')
+NOK = Currency('NOK', 578, 2, 'Norwegian krone')
+NPR = Currency('NPR', 524, 2, 'Nepalese rupee')
+NZD = Currency('NZD', 554, 2, 'New Zealand dollar')
+OMR = Currency('OMR', 512, 3, 'Omani rial')
+PAB = Currency('PAB', 590, 2, 'Panamanian balboa')
+PEN = Currency('PEN', 604, 2, 'Peruvian sol')
+PGK = Currency('PGK', 598, 2, 'Papua New Guinean kina')
+PHP = Currency('PHP', 608, 2, 'Philippine peso')
+PKR = Currency('PKR', 586, 2, 'Pakistani rupee')
+PLN = Currency('PLN', 985, 2, 'Polish złoty')
+PYG = Currency('PYG', 600, 0, 'Paraguayan guaraní')
+QAR = Currency('QAR', 634, 2, 'Qatari riyal')
+RON = Currency('RON', 946, 2, 'Romanian leu')
+RSD = Currency('RSD', 941, 2, 'Serbian dinar')
+RUB = Currency('RUB', 643, 2, 'Russian ruble')
+RWF = Currency('RWF', 646, 0, 'Rwandan franc')
+SAR = Currency('SAR', 682, 2, 'Saudi riyal')
+SBD = Currency('SBD', 90, 2, 'Solomon Islands dollar')
+SCR = Currency('SCR', 690, 2, 'Seychelles rupee')
+SDG = Currency('SDG', 938, 2, 'Sudanese pound')
+SEK = Currency('SEK', 752, 2, 'Swedish krona (plural: kronor)')
+SGD = Currency('SGD', 702, 2, 'Singapore dollar')
+SHP = Currency('SHP', 654, 2, 'Saint Helena pound')
+SLE = Currency('SLE', 925, 2, 'Sierra Leonean leone (new leone)')
+SOS = Currency('SOS', 706, 2, 'Somalian shilling')
+SRD = Currency('SRD', 968, 2, 'Surinamese dollar')
+SSP = Currency('SSP', 728, 2, 'South Sudanese pound')
+STN = Currency('STN', 930, 2, 'São Tomé and Príncipe dobra')
+SVC = Currency('SVC', 222, 2, 'Salvadoran colón')
+SYP = Currency('SYP', 760, 2, 'Syrian pound')
+SZL = Currency('SZL', 748, 2, 'Swazi lilangeni')
+THB = Currency('THB', 764, 2, 'Thai baht')
+TJS = Currency('TJS', 972, 2, 'Tajikistani somoni')
+TMT = Currency('TMT', 934, 2, 'Turkmenistan manat')
+TND = Currency('TND', 788, 3, 'Tunisian dinar')
+TOP = Currency('TOP', 776, 2, 'Tongan paʻanga')
+TRY = Currency('TRY', 949, 2, 'Turkish lira')
+TTD = Currency('TTD', 780, 2, 'Trinidad and Tobago dollar')
+TWD = Currency('TWD', 901, 2, 'New Taiwan dollar')
+TZS = Currency('TZS', 834, 2, 'Tanzanian shilling')
+UAH = Currency('UAH', 980, 2, 'Ukrainian hryvnia')
+UGX = Currency('UGX', 800, 0, 'Ugandan shilling')
+USD = Currency('USD', 840, 2, 'United States dollar')
+USN = Currency('USN', 997, 2, 'United States dollar (next day) (funds code)')
+UYI = Currency('UYI', 940, 0, 'Uruguay Peso en Unidades Indexadas (URUIURUI) (funds code)')
+UYU = Currency('UYU', 858, 2, 'Uruguayan peso')
+UYW = Currency('UYW', 927, 4, 'Unidad previsional')
+UZS = Currency('UZS', 860, 2, 'Uzbekistani sum')
+VED = Currency('VED', 926, 2, 'Venezuelan digital bolívar')
+VES = Currency('VES', 928, 2, 'Venezuelan sovereign bolívar')
+VND = Currency('VND', 704, 0, 'Vietnamese đồng')
+VUV = Currency('VUV', 548, 0, 'Vanuatu vatu')
+WST = Currency('WST', 882, 2, 'Samoan tala')
+XAF = Currency('XAF', 950, 0, 'CFA franc BEAC')
+XAG = Currency('XAG', 961, None, 'Silver (one troy ounce)')
+XAU = Currency('XAU', 959, None, 'Gold (one troy ounce)')
+XBA = Currency('XBA', 955, None, 'European Composite Unit (EURCO) (bond market unit)')
+XBB = Currency('XBB', 956, None, 'European Monetary Unit (E.M.U.-6) (bond market unit)')
+XBC = Currency('XBC', 957, None, 'European Unit of Account 9 (E.U.A.-9) (bond market unit)')
+XBD = Currency('XBD', 958, None, 'European Unit of Account 17 (E.U.A.-17) (bond market unit)')
+XCD = Currency('XCD', 951, 2, 'East Caribbean dollar')
+XDR = Currency('XDR', 960, None, 'Special drawing rights')
+XOF = Currency('XOF', 952, 0, 'CFA franc BCEAO')
+XPD = Currency('XPD', 964, None, 'Palladium (one troy ounce)')
+XPF = Currency('XPF', 953, 0, 'CFP franc (franc Pacifique)')
+XPT = Currency('XPT', 962, None, 'Platinum (one troy ounce)')
+XSU = Currency('XSU', 994, None, 'SUCRE')
+XTS = Currency('XTS', 963, None, 'Code reserved for testing')
+XUA = Currency('XUA', 965, None, 'ADB Unit of Account')
+XXX = Currency('XXX', 999, None, 'No currency')
+YER = Currency('YER', 886, 2, 'Yemeni rial')
+ZAR = Currency('ZAR', 710, 2, 'South African rand')
+ZMW = Currency('ZMW', 967, 2, 'Zambian kwacha')
+ZWG = Currency('ZWG', 924, 2, 'Zimbabwe Gold')
+
+__all__ = [
+          "AED",
+          "AFN",
+          "ALL",
+          "AMD",
+          "ANG",
+          "AOA",
+          "ARS",
+          "AUD",
+          "AWG",
+          "AZN",
+          "BAM",
+          "BBD",
+          "BDT",
+          "BGN",
+          "BHD",
+          "BIF",
+          "BMD",
+          "BND",
+          "BOB",
+          "BOV",
+          "BRL",
+          "BSD",
+          "BTN",
+          "BWP",
+          "BYN",
+          "BZD",
+          "CAD",
+          "CDF",
+          "CHE",
+          "CHF",
+          "CHW",
+          "CLF",
+          "CLP",
+          "CNY",
+          "COP",
+          "COU",
+          "CRC",
+          "CUP",
+          "CVE",
+          "CZK",
+          "DJF",
+          "DKK",
+          "DOP",
+          "DZD",
+          "EGP",
+          "ERN",
+          "ETB",
+          "EUR",
+          "FJD",
+          "FKP",
+          "GBP",
+          "GEL",
+          "GHS",
+          "GIP",
+          "GMD",
+          "GNF",
+          "GTQ",
+          "GYD",
+          "HKD",
+          "HNL",
+          "HTG",
+          "HUF",
+          "IDR",
+          "ILS",
+          "INR",
+          "IQD",
+          "IRR",
+          "ISK",
+          "JMD",
+          "JOD",
+          "JPY",
+          "KES",
+          "KGS",
+          "KHR",
+          "KMF",
+          "KPW",
+          "KRW",
+          "KWD",
+          "KYD",
+          "KZT",
+          "LAK",
+          "LBP",
+          "LKR",
+          "LRD",
+          "LSL",
+          "LYD",
+          "MAD",
+          "MDL",
+          "MGA",
+          "MKD",
+          "MMK",
+          "MNT",
+          "MOP",
+          "MRU",
+          "MUR",
+          "MVR",
+          "MWK",
+          "MXN",
+          "MXV",
+          "MYR",
+          "MZN",
+          "NAD",
+          "NGN",
+          "NIO",
+          "NOK",
+          "NPR",
+          "NZD",
+          "OMR",
+          "PAB",
+          "PEN",
+          "PGK",
+          "PHP",
+          "PKR",
+          "PLN",
+          "PYG",
+          "QAR",
+          "RON",
+          "RSD",
+          "RUB",
+          "RWF",
+          "SAR",
+          "SBD",
+          "SCR",
+          "SDG",
+          "SEK",
+          "SGD",
+          "SHP",
+          "SLE",
+          "SOS",
+          "SRD",
+          "SSP",
+          "STN",
+          "SVC",
+          "SYP",
+          "SZL",
+          "THB",
+          "TJS",
+          "TMT",
+          "TND",
+          "TOP",
+          "TRY",
+          "TTD",
+          "TWD",
+          "TZS",
+          "UAH",
+          "UGX",
+          "USD",
+          "USN",
+          "UYI",
+          "UYU",
+          "UYW",
+          "UZS",
+          "VED",
+          "VES",
+          "VND",
+          "VUV",
+          "WST",
+          "XAF",
+          "XAG",
+          "XAU",
+          "XBA",
+          "XBB",
+          "XBC",
+          "XBD",
+          "XCD",
+          "XDR",
+          "XOF",
+          "XPD",
+          "XPF",
+          "XPT",
+          "XSU",
+          "XTS",
+          "XUA",
+          "XXX",
+          "YER",
+          "ZAR",
+          "ZMW",
+          "ZWG"
+          ]
